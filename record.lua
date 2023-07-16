b5614234774ab8716d347ade7e6657c4e0205caa
Yabastar <138197982+Yabastar@users.noreply.github.com>
2023-07-16 17:40:37 -0500

    Update record.lua

--- record.lua
+++ record.lua
@@ -2,7 +2,7 @@ while true do
 	log = io.open("log", "a")
 	local chat = { os.pullEvent("chat_message") }
 	data = chat[3] .. ": " .. chat[4]
-	log:write(data)
+	log:write(data .. "\n")
 	print(data)
 	log:close()
 end
